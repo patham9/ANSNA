95c664ff211d354213015f72599d31e7ad2a6da2
PtrMan <robertw89@googlemail.com>
2018-10-13 12:54:41 +0200

    Docs: Attention

--- src/Attention.c
+++ src/Attention.c
@@ -9,6 +9,15 @@ Attention Attention_forgetEvent(Attention *taskAttention)
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 Attention Attention_forgetConcept(Attention *conceptAttention, Usage *conceptUsage, long currentTime)
 {
+    /*
+     Usefulness affects absolute forgetting
+     
+     We use instead of a second queue in ALANN a lower priority barrier (very small) dependent on usefulness
+
+     This way it only needs to order according to priority, and items with high usefulness stay even though not in the" attentional focus"
+     (It's what Pei did for quality in OpenNARS)
+     */
+    
     double usefulness = Usage_usefulness(conceptUsage, currentTime);
     double lowerPriorityBarrier = usefulness * USEFULNESS_MAX_PRIORITY_BARRIER;
     return (Attention) { .priority = MAX(lowerPriorityBarrier, conceptAttention->priority * conceptAttention->durability),
